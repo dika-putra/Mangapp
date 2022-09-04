01bcfc39775bbe0d7139b318280e77ac73d148bb
Dika Putra <novianmahardikaputra@gmail.com>
2022-09-04 12:26:28 +0700

    update data

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -32,7 +32,7 @@ class _HomePageState extends State<HomePage> {
       'lastChapterTime': Random().nextInt(60) + 1,
       'genres': Random().nextInt(3) + 1,
       'banner':
-          'https://freshcomics.s3.amazonaws.com/issue_covers/APR162152.jpg',
+          'https://vignette.wikia.nocookie.net/overlord/images/b/bd/Overlord_Anime.jpg/revision/latest?cb=20171205042417&path-prefix=es',
     },
     {
       'title': 'Sword Art Online',
@@ -42,6 +42,33 @@ class _HomePageState extends State<HomePage> {
       'genres': Random().nextInt(3) + 1,
       'banner':
           'https://i0.wp.com/operationrainfall.com/wp-content/uploads/2016/08/Sword_Art_Online_Volume_18_LN_Cover.jpg'
+    },
+    {
+      'title': 'One Piece',
+      'writer': 'Eiichiro Oda',
+      'lastChapter': 1031,
+      'lastChapterTime': Random().nextInt(60) + 1,
+      'genres': Random().nextInt(3) + 1,
+      'banner':
+          'https://upload.wikimedia.org/wikipedia/ar/9/90/One_Piece%2C_Volume_61_Cover_(Japanese).jpg'
+    },
+    {
+      'title': 'Boku no Hero Academia',
+      'writer': 'Kohei Horikoshi',
+      'lastChapter': 13,
+      'lastChapterTime': Random().nextInt(60) + 1,
+      'genres': Random().nextInt(3) + 1,
+      'banner':
+          'https://upload.wikimedia.org/wikipedia/en/thumb/5/5a/Boku_no_Hero_Academia_Volume_1.png/220px-Boku_no_Hero_Academia_Volume_1.png'
+    },
+    {
+      'title': 'Detective Conan',
+      'writer': 'Gosho Aoyama',
+      'lastChapter': 1054,
+      'lastChapterTime': Random().nextInt(60) + 1,
+      'genres': Random().nextInt(3) + 1,
+      'banner':
+          'https://upload.wikimedia.org/wikipedia/en/3/3f/Case_Closed_Volume_36.png'
     },
   ];
 
